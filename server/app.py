21fcf554acb43e2f4dfbabd3a2a2e33db557c5a8
Giuliana Vera <107806539+GiuliV21@users.noreply.github.com>
2024-09-24 00:23:27 -0300

    agrego requeriments, y la vista de crear usuario, login, y agrego los metodos faltantes al index

--- server/app.py
+++ server/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from models import db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:123456@localhost/stockearte'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:giuli123@localhost/stockearte'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
@@ -10,4 +10,4 @@ db.init_app(app)
 # Crea las tablas en la bd
 with app.app_context():
     db.drop_all()  # Elimina todas las tablas
-    db.create_all()  
+    db.create_all()  # las crea nuevamente
